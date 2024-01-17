d712374354800603e5ab927a117c369740d546bf
VitaliyD3201 <Vitaliy.net.pnz@gmail.com>
2024-01-17 15:26:28 +0300

    HomeWork

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,12 +1,12 @@
-import com.codeborne.selenide.CollectionCondition;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+
+import data.Language;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -14,43 +14,43 @@ import static com.codeborne.selenide.Selenide.*;
 public class MyTest {
 
     @BeforeEach
-    void setUp() {
-        open("https://duckduckgo.com/");
-    }
-
-
-    @Test
-    @Tag("block")
-    @DisplayName("для поискового запроса отображается не пустой список")
-    void successfulSearchTest() {
-        $("#searchbox_input").setValue("Steezy").pressEnter();
-        $$("[data-testid=mainline] li[data-layout=organic]").shouldBe(
-                CollectionCondition.sizeGreaterThan(0));
-
+    void setUp()
+    {
+        open("https://www.startpage.com/");
     }
 
     @ValueSource(strings = {
-            "sunday","monday"
+            "Selenide", "JUnit 5", "Allure"
     })
-    @ParameterizedTest(name = "Параметризация для поискового запроса {0} отображается не пустой список")
-    @Tag("block")
-    void successfulSearchTestParameterized(String searchQuery) {
-        $("#searchbox_input").setValue(searchQuery).pressEnter();
-        $$("[data-testid=mainline] li[data-layout=organic]").shouldBe(
-                CollectionCondition.sizeGreaterThan(0));
-
+    @ParameterizedTest(name="For a search query, {0} should not give an empty list of cards")
+    @Tag("BLOCKER")
+    void searchResultsShouldNotBeEmptyTest(String searchQuery){
+        $("#q").setValue(searchQuery).pressEnter();
+        $$("[class='w-gl__result__main']")
+                .shouldBe(sizeGreaterThan(0));
     }
 
     @CsvSource(value = {
-            "Selenide , https://selenide.org",
-            "JUnit 5 , https://junit.org"
+            "Selenide, https://selenide.org/",
+            "JUnit 5, https://junit.org"
     })
-    @ParameterizedTest(name = "Для поискового запроса {0} в первой карточке должна быть ссылка {1}")
+    @ParameterizedTest(name="For a search query {0}, the first card must have a link {1}")
     @Tag("BLOCKER")
-    void searchResultsShouldContainExpectedUrl(String searchQuery, String expectedLink) {
-        $("#searchbox_input").setValue(searchQuery).pressEnter();
-        $("[data-testid='mainline'] li[data-layout='organic']")
+    void searchResultsShouldContainExpectedUrlTest(String searchQuery, String expectedLink){
+        $("#q").setValue(searchQuery).pressEnter();
+        $("[class='w-gl__result-url result-link']")
                 .shouldHave(text(expectedLink));
+    }
+
+    @Disabled("421")
+    @Test
+    @Tag("BLOCKER")
+    @EnumSource(Language.class)
+    @ParameterizedTest (name="When searching, the search bar should not be empty")
+    void successfulSearchTwoTest(Language language){
+        System.out.println(language.description);
+        $("#q").setValue(language.description).pressEnter();
+        $("[class='sx-kp-short-extract sx-kp-short-extract-nontruncated']").shouldHave(text(language.description));
     }
 
 
--- src/test/java/SelenideWebTest.java
+++ src/test/java/SelenideWebTest.java
@@ -1,9 +1,6 @@
 import com.codeborne.selenide.CollectionCondition;
 import data.Language;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.*;
 import java.util.List;
@@ -37,6 +34,7 @@ public class SelenideWebTest {
                 )
         );
     }
+
 
     @MethodSource
     @ParameterizedTest
